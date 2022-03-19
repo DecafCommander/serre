c26ecd3bcd2f45cad550ec0435caaaff973e84ee
DecafCommander <100421715+DecafCommander@users.noreply.github.com>
2022-03-19 13:16:22 +0100

    Plant Bewatering

--- main.py
+++ main.py
@@ -1,42 +1,57 @@
-def on_pin_pressed_p2():
-    basic.show_leds("""
-        . # . # .
-                # . # . #
-                . # # # .
-                # . # . #
-                . # . # .
-    """)
-    ContinuousServo.spin_one_way_with_speed(AnalogPin.P0, 100)
-    basic.pause(500)
-    ContinuousServo.turn_off_motor(DigitalPin.P0)
-    basic.pause(59000)
-    ContinuousServo.spin_one_way_with_speed(AnalogPin.P0, 100)
-    basic.pause(500)
-    ContinuousServo.turn_off_motor(DigitalPin.P0)
-input.on_pin_pressed(TouchPin.P2, on_pin_pressed_p2)
+reading = 0
 
-def on_every_interval():
+def on_forever():
+    vochtigheid = 0
+    if vochtigheid < 50:
+        ContinuousServo.spin_one_way_with_speed(AnalogPin.P0, 100)
+        basic.pause(500)
+        ContinuousServo.turn_off_motor(DigitalPin.P0)
+        basic.pause(1800000)
+        ContinuousServo.spin_other_way_with_speed(AnalogPin.P0, 100)
+        basic.pause(500)
+        ContinuousServo.turn_off_motor(DigitalPin.P0)
+        basic.pause(5000)
+basic.forever(on_forever)
+
+def on_forever2():
+    if input.light_level() < 150:
+        ContinuousServo.spin_one_way_with_speed(AnalogPin.P2, 100)
+        basic.pause(500)
+        ContinuousServo.turn_off_motor(DigitalPin.P2)
+        basic.pause(1800000)
+        ContinuousServo.spin_other_way_with_speed(AnalogPin.P2, 100)
+        basic.pause(500)
+        ContinuousServo.turn_off_motor(DigitalPin.P2)
+        basic.pause(5000)
+basic.forever(on_forever2)
+
+def on_forever3():
     if input.temperature() < 20:
-        basic.show_leds("""
-            # . # . #
-                        . # # # .
-                        # # # # #
-                        . # # # .
-                        # . # . #
-        """)
         ContinuousServo.spin_one_way_with_speed(AnalogPin.P0, 100)
         basic.pause(500)
         ContinuousServo.turn_off_motor(DigitalPin.P0)
-        servos.P1.set_angle(90)
-        basic.pause(59000)
-        ContinuousServo.spin_one_way_with_speed(AnalogPin.P0, 100)
+        ContinuousServo.spin_one_way_with_speed(AnalogPin.P1, 100)
+        basic.pause(1800000)
+        ContinuousServo.turn_off_motor(DigitalPin.P1)
+        ContinuousServo.spin_other_way_with_speed(AnalogPin.P0, 100)
         basic.pause(500)
         ContinuousServo.turn_off_motor(DigitalPin.P0)
-    basic.show_leds("""
-        . # . # .
-                # # . # #
-                . . # . .
-                # # . # #
-                . # . # .
-    """)
-loops.every_interval(60000, on_every_interval)
+        basic.pause(5000)
+basic.forever(on_forever3)
+
+def on_forever4():
+    global reading
+    pins.analog_write_pin(AnalogPin.P1, 1023)
+    reading = pins.analog_read_pin(AnalogPin.P0)
+    pins.analog_write_pin(AnalogPin.P1, 0)
+    led.plot_bar_graph(reading, 1023)
+    if input.button_is_pressed(Button.A):
+        basic.show_number(reading)
+    if reading < 500:
+        pins.servo_write_pin(AnalogPin.P2, 0)
+        basic.pause(3000)
+        pins.servo_write_pin(AnalogPin.P2, 80)
+        basic.pause(3000)
+        pins.analog_write_pin(AnalogPin.P0, 0)
+    basic.pause(5000)
+basic.forever(on_forever4)
--- main.ts
+++ main.ts
@@ -1,5 +1,19 @@
+let reading = 0
 basic.forever(function () {
-    if (input.lightLevel() > 150) {
+    let vochtigheid = 0
+    if (vochtigheid < 50) {
+        ContinuousServo.spin_one_way_with_speed(AnalogPin.P0, 100)
+        basic.pause(500)
+        ContinuousServo.turn_off_motor(DigitalPin.P0)
+        basic.pause(1800000)
+        ContinuousServo.spin_other_way_with_speed(AnalogPin.P0, 100)
+        basic.pause(500)
+        ContinuousServo.turn_off_motor(DigitalPin.P0)
+        basic.pause(5000)
+    }
+})
+basic.forever(function () {
+    if (input.lightLevel() < 150) {
         ContinuousServo.spin_one_way_with_speed(AnalogPin.P2, 100)
         basic.pause(500)
         ContinuousServo.turn_off_motor(DigitalPin.P2)
@@ -7,10 +21,11 @@ basic.forever(function () {
         ContinuousServo.spin_other_way_with_speed(AnalogPin.P2, 100)
         basic.pause(500)
         ContinuousServo.turn_off_motor(DigitalPin.P2)
+        basic.pause(5000)
     }
 })
 basic.forever(function () {
-    if (input.temperature() > 20) {
+    if (input.temperature() < 20) {
         ContinuousServo.spin_one_way_with_speed(AnalogPin.P0, 100)
         basic.pause(500)
         ContinuousServo.turn_off_motor(DigitalPin.P0)
@@ -20,17 +35,26 @@ basic.forever(function () {
         ContinuousServo.spin_other_way_with_speed(AnalogPin.P0, 100)
         basic.pause(500)
         ContinuousServo.turn_off_motor(DigitalPin.P0)
+        basic.pause(5000)
     }
 })
 basic.forever(function () {
-    let vochtigheid = 0
-    if (vochtigheid > 50) {
-        ContinuousServo.spin_one_way_with_speed(AnalogPin.P0, 100)
-        basic.pause(500)
-        ContinuousServo.turn_off_motor(DigitalPin.P0)
-        basic.pause(1800000)
-        ContinuousServo.spin_other_way_with_speed(AnalogPin.P0, 100)
-        basic.pause(500)
-        ContinuousServo.turn_off_motor(DigitalPin.P0)
+    pins.analogWritePin(AnalogPin.P1, 1023)
+    reading = pins.analogReadPin(AnalogPin.P0)
+    pins.analogWritePin(AnalogPin.P1, 0)
+    led.plotBarGraph(
+    reading,
+    1023
+    )
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showNumber(reading)
     }
+    if (reading < 500) {
+        pins.servoWritePin(AnalogPin.P2, 0)
+        basic.pause(3000)
+        pins.servoWritePin(AnalogPin.P2, 80)
+        basic.pause(3000)
+        pins.analogWritePin(AnalogPin.P0, 0)
+    }
+    basic.pause(5000)
 })
